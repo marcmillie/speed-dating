709e8e9dd19dfd945ab20226bff9862bfaf94cfd
marcmillie <mrmarcusmiller0@gmail.com>
2024-02-05 00:42:28 -0500

    basic title and buttons added

---
+++ speed-dating/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Speed Dating</h1>
+
+      <button>Sign in</button>
+      <button>Log in</button>
+      <a href="">Forgot Password</a>
+    </div>
+  );
+}
+
+export default App;
